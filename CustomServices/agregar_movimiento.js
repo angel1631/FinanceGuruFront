f4cdd7e398503af259b5438d873868ef829cd985
Angel Guillen <angel1631@gmail.com>
2024-05-30 17:58:01 -0600

    se inicia el proyecto

---
+++ CustomServices/agregar_movimiento.js
@@ -0,0 +1,285 @@
+import { useEffect, useState } from "react";
+import { communication } from "../../Core/scripts/communication.js";
+import { GCard } from "../../Core/components/GCard.js";
+import { GModal } from "../../Core/components/GModal.js";
+import { GForm } from "../../Core/components/GForm.js";
+import { validate_form } from "../../Core/scripts/form.js";
+import { cast_money } from "../../Core/scripts/casts.js";
+import { getContrast } from "../../Core/scripts/color.js";
+
+
+
+function AddMovimiento({after_save}){
+    let values = useState({});
+    let show_form_account = useState(false);
+    let show_form_tag = useState(false);
+    let show_form_sub_tag = useState(false);
+    let account_scheme = useState({});
+    let tag_scheme = useState({});
+    let tag_values = useState({});
+    let sub_tag_scheme = useState({});
+    let sub_tag_values = useState({});
+    let account_values = useState({});
+    let accounts = useState([]);
+    let tags = useState([]);
+    let sub_tags = useState([]);
+    let now = new Date();
+    let day = ("0" + now.getDate()).slice(-2);
+    let month = ("0" + (now.getMonth() + 1)).slice(-2);
+
+    let today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+    let mov_value = useState({description:'',amount:'', account:'', tag:'', fecha:today});
+    let show_avanzadas = useState(false);
+
+    useEffect(()=>{
+        get_account_scheme();
+        get_tag_scheme();
+        get_sub_tag_scheme();
+        get_accounts();
+        get_sub_tags();
+        get_tags();
+    },[]);
+    async function get_accounts(){
+        const data = await communication({url:`/api/FinanceGuru/Services/user_accounts`});
+        accounts[1](data);
+    }
+    async function get_sub_tags(){
+        const data = await communication({url:`/api/FinanceGuru/Services/user_sub_tags`});
+        sub_tags[1](data);
+    }
+    async function get_tags(){
+        const data = await communication({url:`/api/FinanceGuru/Services/user_tags?expense=si`});
+        tags[1](data);
+    }
+    async function get_account_scheme(){
+        try{
+            let scheme_json = await communication({url:`/api/FinanceGuru/FGCuenta/scheme`});
+            console.log("----scheme_json", scheme_json);
+            account_scheme[1](scheme_json)
+        }catch(error){
+            console.log("Error en api", error);
+        }
+    }
+    async function get_tag_scheme(){
+        try{
+            let scheme_json = await communication({url:`/api/FinanceGuru/FGTag/scheme`});
+            console.log("----scheme_json", scheme_json);
+            tag_scheme[1](scheme_json)
+        }catch(error){
+            console.log("Error en api", error);
+        }
+    }
+    async function get_sub_tag_scheme(){
+        try{
+            let scheme_json = await communication({url:`/api/FinanceGuru/FGSubTag/scheme`});
+            console.log("----scheme_json", scheme_json);
+            sub_tag_scheme[1](scheme_json)
+        }catch(error){
+            console.log("Error en api", error);
+        }
+    }
+    
+    async function save_new_account(){
+        try{
+            await validate_form(account_values[0], account_scheme[0].fields);
+            let url = '/api/FinanceGuru/FGCuenta/insert';
+            let respuesta_json = await communication({url, data: account_values[0]});
+            accounts[1]([...accounts[0],{...account_values[0],id: respuesta_json.id}]);
+            show_form_account[1](false);
+            alert("Todo Ok");
+        }catch(err){
+            alert(err);
+        }
+        
+    }
+    
+    async function save_new_tag(){
+        try{
+            await validate_form(tag_values[0], tag_scheme[0].fields);
+            let url = '/api/FinanceGuru/FGTag/insert';
+            let respuesta_json = await communication({url, data: tag_values[0]});
+            tags[1]([...tags[0],{...tag_values[0],id: respuesta_json.id}]);
+            show_form_tag[1](false);
+            alert("Todo Ok");
+            
+        }catch(err){
+            alert(err);
+        } 
+    }
+    async function save_new_sub_tag(){
+        try{
+            await validate_form(sub_tag_values[0], sub_tag_scheme[0].fields);
+            let url = '/api/FinanceGuru/FGSubTag/insert';
+            let respuesta_json = await communication({url, data: sub_tag_values[0]});
+            sub_tags[1]([...sub_tags[0],{...sub_tag_values[0],id: respuesta_json.id}]);
+            show_form_sub_tag[1](false);
+            alert("Todo Ok");
+            
+        }catch(err){
+            alert(err);
+        } 
+    }
+    async function save_movimiento(){
+        
+        try{
+            if(mov_value[0].description=='') throw 'Se debe definir una descripcion';
+            if(mov_value[0].amount=='') throw 'Se debe definir el monto';
+            if(mov_value[0].account=='') throw 'Se debe seleccionar una cuenta';
+            if(mov_value[0].tag=='') throw 'Se debe seleccionar una clasificacion';
+            let movimiento = {...mov_value[0], is_debit:'si'};
+            if(!movimiento.sub_tag) movimiento.sub_tag = sub_tags[0][0].id; 
+            let url = '/api/FinanceGuru/Services/save_movimiento';
+            let respuesta_json = await communication({url, data: movimiento});
+            let new_accounts = accounts[0].map((e)=>{
+                if(e.id==mov_value[0].account) e.balance = e.balance - mov_value[0].amount;
+                return e;
+            });
+            accounts[1](new_accounts);
+            alert(respuesta_json.msj);
+            after_save();
+        }catch(err){
+            alert(err);
+        }
+        
+        
+    }
+    async function select_account(id){
+        if(mov_value[0].account!=id){
+            let account_active = document.getElementsByClassName('account_container active');
+            if(account_active.length>0){account_active[0].classList.remove('active')};
+            document.getElementById(id).classList.add('active');
+            mov_value[1](prev_value=>({...prev_value, account: id}));
+        }else{
+            document.getElementById(id).classList.remove('active');
+            mov_value[1](prev_value=>({...prev_value, account: ''}));
+        }
+    }
+    async function select_tag(id){
+        if(mov_value[0].tag!=id){
+            let tag_active = document.getElementsByClassName('tag_container active');
+            if(tag_active.length>0){tag_active[0].classList.remove('active')};
+            document.getElementById(id).classList.add('active');
+            mov_value[1](prev_value=>({...prev_value, tag: id}));
+        }else{
+            document.getElementById(id).classList.remove('active');
+            mov_value[1](prev_value=>({...prev_value, tag: ''}));
+        }
+    }
+
+    async function select_sub_tag(id){
+        if(mov_value[0].sub_tag!=id){
+            let sub_tag_active = document.getElementsByClassName('sub_tag_container active');
+            if(sub_tag_active.length>0){sub_tag_active[0].classList.remove('active')};
+            document.getElementById(id).classList.add('active');
+            mov_value[1](prev_value=>({...prev_value, sub_tag: id}));
+        }else{
+            document.getElementById(id).classList.remove('active');
+            mov_value[1](prev_value=>({...prev_value, sub_tag: ''}));
+        }
+    }
+    function onChange(id,value){
+        mov_value[1](prev_value=>({...prev_value, [id]: value}));
+    }
+    return (
+        <>
+        
+        <GCard className="movimientos-container">
+            <div className="buttons_container">
+                    <button className="add_account button btn-add" onClick={()=>{show_form_account[1](true);}}>Agregar Cuenta</button>
+                    <button className="add_tag button button btn-add" onClick={()=>{show_form_tag[1](true);}}>Agregar Clasificacion</button>
+                    <button className="add_sub_tag button button btn-add" onClick={()=>{show_form_sub_tag[1](true);}}>Agregar Sub Clasificacion</button>
+            </div>
+            <div className="accounts_container">
+                <div className="accounts_container_title">
+                    Pagará con: 
+                </div>
+                <div className="accounts_container_options">
+                    {accounts[0] && accounts[0].map((e)=>(
+                        <div className="account_container" id={e.id} onClick={()=>{select_account(e.id)}} >
+                            <div className="account_title">{e.title}</div>
+                            <div className="account_icon"><i className="material-icons-outlined" style={{color: e.color}} >{e.icon}</i></div>
+                            <div className="account_balance">Q. {cast_money({amount: e.balance})}</div>
+                        </div>    
+                    ))}
+                </div>
+                
+            </div>
+            <div className="monto_container GForm">
+                <div className="gform-line">
+                    <textarea type="text" id="description" value={mov_value[0].description} onChange={(e)=>{onChange('description', e.target.value)}}/>
+                    <label htmlFor="description" className="field-description">Descripcion</label>
+                </div>
+                <div className="gform-line">
+                    <input type="number" id="amount" value={mov_value[0].amount} onChange={(e)=>{onChange('amount', e.target.value)}}/>
+                    <label htmlFor="amount" className="field-description">Monto</label>
+                </div>
+                <div className="gform-line">
+                    <input type="date" id="fecha" value={mov_value[0].fecha} onChange={(e)=>{onChange('fecha', e.target.value)}}/> 
+                </div>
+                <button className="send button" onClick={save_movimiento}>Guardar</button>
+            </div>
+            <h2>Seleccionar la clasificacion</h2>
+            <div className="tags_container">
+                {tags[0] && tags[0].map((e)=>{
+                    let contraste = getContrast(e.color, '#FFFFFF');
+                    let new_style = {background: e.color};
+                    if(contraste>4) new_style.color = "#FFFFFF";
+                    return (
+                    <div className="tag_container" style={new_style} id={e.id} onClick={()=>{select_tag(e.id)}} >
+                        <div className="tag_icon"><i  className="material-icons-outlined">{e.icon}</i></div>
+                        <div className="tag_title">{e.title}</div>
+                    </div>    
+                )})}
+            </div>
+            <div className="btn_avanzadas" onClick={()=>{show_avanzadas[1](!show_avanzadas[0]);}}><i className="material-icons">{show_avanzadas[0]?"keyboard_arrow_up":"keyboard_arrow_down"}</i></div>
+            {show_avanzadas[0] && 
+                <div className="sub_tags_container">
+                    {sub_tags[0] && sub_tags[0].map((e)=>(
+                        <div className="sub_tag_container" id={e.id} onClick={()=>{select_sub_tag(e.id)}} >
+                            <div className="tag_icon"><i style={{color: e.color}} className="material-icons">{e.icon}</i></div>
+                            <div className="tag_title">{e.title}</div>
+                        </div>    
+                    ))}
+                </div>
+            }
+            
+        </GCard>
+        {show_form_account[0] &&
+            <GModal show={show_form_account} title={`Agregar Cuenta`}>
+                <GForm 
+                    scheme={account_scheme[0]} 
+                    values={account_values}
+                    onSubmit={save_new_account} 
+                    action={'insert'}
+                    primary_action={'insert'}/>
+                
+            </GModal>
+        }
+        {show_form_tag[0] &&
+            <GModal show={show_form_tag} title={`Agregar Clasificacion`}>
+                <GForm 
+                    scheme={tag_scheme[0]} 
+                    values={tag_values}
+                    onSubmit={save_new_tag} 
+                    action={'insert'}
+                    primary_action={'insert'}/>
+                
+            </GModal>
+        }
+        {show_form_sub_tag[0] &&
+            <GModal show={show_form_sub_tag} title={`Agregar Clasificacion`}>
+                <GForm 
+                    scheme={sub_tag_scheme[0]} 
+                    values={sub_tag_values}
+                    onSubmit={save_new_sub_tag} 
+                    action={'insert'}
+                    primary_action={'insert'}/>
+                
+            </GModal>
+        }
+        </>
+    );
+}
+
+
+export {AddMovimiento}
---
+++ CustomServices/main.js
@@ -0,0 +1,162 @@
+import { useEffect, useState } from "react";
+import { GCard } from "../../Core/components/GCard";
+import { cookie_in_cookies_string } from "../../Core/scripts/cookie";
+import { communication } from "../../Core/scripts/communication";
+import { GModal } from "../../Core/components/GModal";
+import { AddMovimiento } from "./agregar_movimiento";
+import { cast_money } from "../../Core/scripts/casts";
+import { getContrast } from "../../Core/scripts/color";
+
+export async function server_props(context){
+    try{
+        let session_token = cookie_in_cookies_string({name: 'session_token',cookies_string: context.req.headers.cookie});
+        let now = new Date();
+        let start = (new Date(now.getFullYear(), now.getMonth(), 1)).toISOString();
+        let end = (new Date(now.getFullYear(), now.getMonth()+1,0)).toISOString();
+        //let start = `${now.getFullYear()}-${((now.getMonth())+1)<10?"0"+((now.getMonth())+1):((now.getMonth())+1)}-01`;
+        let data = {start,end};
+        console.log("iiiidata",data)
+        const resumen_clasificacion = await communication({url:`/api/FinanceGuru/Services/resumen_movimientos`, data,session_token});
+        
+        console.log("oooooooooooooooresumen",resumen_clasificacion);
+        return {resumen: resumen_clasificacion};
+    }catch(error){
+        console.log("Error SSR: ",error);
+        return { props: { } }
+    }
+}
+export default function main({server_props}){
+  
+    let resumen = useState(server_props.resumen?server_props.resumen.map(r=>({...r,movimientos:[]})):[]);
+    let data = useState()
+    let show_form_movimientos = useState(false);
+    let now = new Date();
+    let fecha_referencia = useState({start: (new Date(now.getFullYear(), now.getMonth(), 1)).toISOString(), end: (new Date(now.getFullYear(), now.getMonth()+1,0)).toISOString()})
+    
+    function grafica(){
+      let grafica_div =  echarts.init(document.getElementById('grafica_resumen'));
+      var options = {
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          top: '5%',
+          left: 'center'
+        },
+        series: [
+          {
+            name: 'Gastos',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            itemStyle: {
+              borderRadius: 10,
+              borderColor: '#fff',
+              borderWidth: 2
+            },
+            colorBy:'data',
+            label: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: 30,
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data: resumen[0].map(r=>({value:r.balance,name:r.title,itemStyle:{color:r.color}})) 
+          }
+        ]
+      };
+      grafica_div.setOption(options);
+    }
+    useEffect(()=>{
+      
+      grafica();
+    },[resumen])
+    async function get_resumen(){
+      let now = new Date();
+        let start = (new Date(now.getFullYear(), now.getMonth(), 1)).toISOString();
+        let end = (new Date(now.getFullYear(), now.getMonth()+1,0)).toISOString();
+        //let start = `${now.getFullYear()}-${((now.getMonth())+1)<10?"0"+((now.getMonth())+1):((now.getMonth())+1)}-01`;
+        let data = {start,end};
+        const resumen_clasificacion = await communication({url:`/api/FinanceGuru/Services/resumen_movimientos`, data});
+      resumen[1](resumen_clasificacion.map(r=>({...r,movimientos:[]})))
+    }
+    async function cargar_movimientos(id,el){
+      
+      if(el.target.textContent.indexOf('down')>=0){
+        let url = '/api/FinanceGuru/Services/buscar_movimientos';
+        let respuesta_json = await communication({url, data: {TagId: id, start: fecha_referencia[0].start,end:fecha_referencia[0].end}});
+        let new_resumen = resumen[0].map((e)=>{
+          if(e.id==id)
+            return {...e,movimientos:respuesta_json};
+          else return e;
+        });
+        resumen[1](new_resumen);
+        el.target.textContent = 'arrow_drop_up';
+      }else{
+        el.target.textContent = 'arrow_drop_down';
+        let new_resumen = resumen[0].map((e)=>{
+          if(e.id==id)
+            return {...e,movimientos:[]};
+          else return e;
+        });
+        resumen[1](new_resumen)
+      }
+    }
+    function onClouseAddMovimiento(){
+      get_resumen();
+      grafica();
+    }
+    function after_save_movimiento(){
+      show_form_movimientos[1](false);
+      get_resumen();
+    }
+    return (
+        <div>
+            {show_form_movimientos[0] && 
+              <GModal show={show_form_movimientos} title={`Agregar Movimientos`} onClouse={onClouseAddMovimiento}>
+                  <AddMovimiento after_save={after_save_movimiento}/>
+              </GModal>
+            }
+            <GCard>
+                <div className="button btn-add sm-circle-btn btn-smsd" onClick={(e)=>{show_form_movimientos[1](true)}}> <i className="material-icons-outlined">add</i></div>
+                <div id="grafica_resumen" className="grafica_resumen" ></div>
+            </GCard>
+            {resumen[0].map(e=>{
+              let contraste = getContrast(e.color, '#FFFFFF');
+              let new_style = {background: e.color};
+              if(contraste>4) new_style.color = "#FFFFFF";
+              return (
+                <div className="resumen_tag" style={new_style} >
+                    <div className="head_resumen_tag">
+                      <i className="material-icons-outlined">{e.icon}</i>
+                      <label className="title">{e.title}</label>
+                      <label className="balance">Q. {cast_money({amount:e.balance})}</label>
+                      <div className="expandir" onClick={(c)=>{cargar_movimientos(e.id,c)}}>
+                        <i className="material-icons-outlined icon_movimientos">arrow_drop_down</i>
+                      </div>
+                    </div>
+                    {e.movimientos.length>0 &&
+                      <div className="detalle_resumen_tag">
+                        {e.movimientos.map((m,index)=>(
+                          <div className="movimiento" key={index}>
+                            <label>{m.fecha}</label>
+                            <label>{m.description}</label>
+                            <label>Q. {cast_money({amount: m.amount})}</label>
+                          </div>
+                        ))}
+                      </div>
+                    }
+                </div>
+            )})}
+            
+        </div>
+    );
+}
