dd6c068d4a07547fb2210d24600e4744db1267e6
Angel Guillen <angel1631@gmail.com>
2024-10-02 23:03:12 -0600

    se agrego el tutorial y se le coloco id a varios elementos

--- CustomServices/main.js
+++ CustomServices/main.js
@@ -164,7 +164,7 @@ export default function main({server_props}){
 
       ];
       const tour = new SimpleTour(steps);
-      //tour.start();
+      tour.start();
       console.log("En el dom esta", document.querySelector("#add_move_button"));
     }
   },[is_ready])
