da9891bc146012b7f66334697ae32377e684b86c
Angel Guillen <angel1631@gmail.com>
2024-10-08 23:16:03 -0600

    fix

--- CustomServices/main.js
+++ CustomServices/main.js
@@ -342,7 +342,7 @@ export default function main({server_props}){
       }else if(view=='group'){
         selected_view[1]('group')
       }
-      get_resumen();
+      get_resumen(undefined,undefined,undefined, view);
     }
     function set_today(){
       let now = new Date();
